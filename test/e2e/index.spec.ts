99e663a414e9b4c37ac9da552776a6833c29da5b
Drew Immerman <drew@stateless.studio>
2026-05-09 14:50:57 -0400

    tests: fix tests

--- test/e2e/index.spec.ts
+++ test/e2e/index.spec.ts
@@ -4,5 +4,5 @@ test('homepage has expected title', async ({ page }) => {
 	await page.goto('/');
 
 	// Adjust this according to your actual index.html title
-	await expect(page).toHaveTitle(/.*@stateless-studio\/ui-react-demo.*/);
+	await expect(page).toHaveTitle(/.*Stateless Studio UI.*/);
 });
